50e8fd520884686ea0b45a18207eb14357d0cf6f
trugrav <eric@etduke.com>
2015-09-04 16:01:31 -0500

    troubleshooting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: => 'tgv-pinteresting.herokuapp.com' }
+  # Devise Requested this addition
+  config.action_mailer.default_url_options = { host: '#' }
+
 end
