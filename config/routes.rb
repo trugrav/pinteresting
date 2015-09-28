f63b66e3a082ab1158fa537f652606e3445d2444
trugrav <eric@etduke.com>
2015-09-28 13:03:29 -0500

    update show view and changed root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :pins
   devise_for :users
   get 'pages/home'
-  root "pages#home"
+  root "pins#index"
   get "about" => "pages#about"
   get "note" => "pages#note"
   # The priority is based upon order of creation: first created -> highest priority.
