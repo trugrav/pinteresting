42f0e1192882878c56fcd98f846a21b1fbafc382
trugrav <eric@etduke.com>
2015-09-28 14:40:08 -0500

    add names to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :pins
+
+  validates :name, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :name, uniqueness: true
+  validates :name, length: { maximum: 15 }
+  validates :password, length: { minimum: 6..20 }
 end
